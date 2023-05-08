43642b1af2b4f7e06d0d5ebc2af2b70b3f1899b3
Tyler Lin <tylerlin0820@gmail.com>
2023-05-08 16:40:05 -0400

    Update Calculator.py
    edits

--- Calculator.py
+++ Calculator.py
@@ -6,8 +6,14 @@ window = tk.Tk()
 window.title('PyCalculator')
 window.geometry('500x600')
 
+entry = ttk.Entry(master = window)
+entry.pack()
+
 button = ttk.Button(master = window, text = 'Enter')
 button.pack()
+
+label = ttk.Label(master = window, text = '^')
+label.pack()
 
 # should run LAST
 # mainloop updates the GUI and checks for events
