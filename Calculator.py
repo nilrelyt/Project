cacf2f31b0aefd0a745fc29fa29f2b8dc016eb49
Tyler Lin <tylerlin0820@gmail.com>
2023-05-08 16:01:09 -0400

    Update Calculator.py
    button test

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,14 @@
 import tkinter as tk
+from tkinter import ttk
 
 # create window
 window = tk.Tk()
 window.title('PyCalculator')
 window.geometry('500x600')
+
+button = ttk.Button(master = window, text = 'Enter')
+button.pack()
+
 # should run LAST
 # mainloop updates the GUI and checks for events
 window.mainloop()
