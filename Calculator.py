61d54ad3db6f45eb36487be6109574dc8bdf3482
Gao, Wilson <wilsongao2002@gmail.com>
2023-05-08 00:15:56 -0400

    created window

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# create window
+window = tk.Tk()
+window.title('PyCalculator')
+window.geometry('500x600')
+# should run LAST
+# mainloop updates the GUI and checks for events
+window.mainloop()
